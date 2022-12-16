a94a2c609374fbfed74283c6f05c23a0584c903d
keeprich <35325509+keeprich@users.noreply.github.com>
2022-12-16 04:51:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,4 +7,10 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = ami-01b5ec3ed8678d8b7
   key_name = "name=kenneth"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "Terraform-server"
+    "team" = "DevOps"
+    "env" = "Dev"
+  }
 }
